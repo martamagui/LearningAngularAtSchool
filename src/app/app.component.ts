37b17d7eeb77436ea01238a5f3727d1009ad4a1f
martamagui <marta.molina@live.u-tad.com>
2022-01-14 12:33:23 +0100

    Primeros pasos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ejemplo';
+  nombre = "MartaMagui";
+  edad = 23;
+  email = "mi.email@email.com";
+  sueldos = [1500,1700, 2100];
+  activo = true;
+  esActivo(){
+    if(this.activo){
+      return 'Trabajador activo';
+    }else{
+      return 'Trabajador inactivo';
+    }
+  }
+  sumaSueldos(){
+    let suma = 0 ; 
+    for(let i = 0 ; i<this.sueldos.length;++i){
+      suma+= this.sueldos[i];
+    }
+    return suma;
+  }
+  // Contador
+  contador=1;
+  incrementar(){
+    this.contador++;
+  }
+  decrementar(){
+    this.contador--;
+  }
+
 }
